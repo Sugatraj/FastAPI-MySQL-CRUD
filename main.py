a2e935510c12811feba89cf2d7b060fb8a182703
Sugatraj <sugatraj.2106@gmail.com>
2025-06-22 17:10:32 +0530

    Refactor: Organize import statements and add path configuration

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from fastapi import FastAPI, HTTPException, Depends, status, responses
 from pydantic import BaseModel
 from typing import Annotated
