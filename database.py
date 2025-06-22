648d8fc25272f93b5a8d4d2c69e6dccce9aab890
Sugatraj <sugatraj.2106@gmail.com>
2025-06-22 13:11:06 +0530

    Add .env to .gitignore and load environment variables in database.py

--- database.py
+++ database.py
@@ -1,8 +1,13 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
 
-URL_DATABASE = 'mysql+pymysql://root:SugaSensei%402025@localhost:3306/blogapplication'
+load_dotenv()
+
+# URL_DATABASE = 'mysql+pymysql://root:SugaSensei%402025@localhost:3306/blogapplication'
+URL_DATABASE = os.getenv("URL_DATABASE")
 
 engine = create_engine(URL_DATABASE)
 
